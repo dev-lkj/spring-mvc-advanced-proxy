4eed8f1e0b11e13c93d5af896d1810172eb919ee
dev-lkj <2_kj@daum.net>
2024-03-23 20:43:40 +0900

    proxy, decorator pattern add

---
+++ src/main/java/hello/proxy/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,27 @@
+package hello.proxy.app.v2;
+
+import hello.proxy.app.v1.OrderRepositoryV1;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class OrderRepositoryV2 {
+
+
+    public void save(String itemId) {
+        //저장 로직
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try{
+            Thread.sleep(millis);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+
